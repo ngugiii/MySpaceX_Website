9dd96c29907b6c7081bfd0c3ba896103643ff99c
ngugiii <108797418+ngugiii@users.noreply.github.com>
2023-03-02 17:14:15 +0300

    changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -16,21 +16,21 @@ const Footer = () => {
         <div className="footer-box">
           <h4>Useful links</h4>
           <div className="footer-links">
-            <a href="#">&bull; Support</a>
-            <a href="#">&bull; About</a>
-            <a href="#">&bull; Learn</a>
-            <a href="#">&bull; Hosting</a>
-            <a href="#">&bull; Messenger</a>
+            <a href="">&bull; Support</a>
+            <a href="">&bull; About</a>
+            <a href="">&bull; Learn</a>
+            <a href="">&bull; Hosting</a>
+            <a href="">&bull; Messenger</a>
           </div>
         </div>
         <div className="footer-box">
         <h4>Support</h4>
           <div className="footer-links">
-            <a href="#">&bull; Support</a>
-            <a href="#">&bull; About</a>
-            <a href="#">&bull; Learn</a>
-            <a href="#">&bull; Hosting</a>
-            <a href="#">&bull; Messenger</a>
+            <a href="">&bull; Support</a>
+            <a href="">&bull; About</a>
+            <a href="">&bull; Learn</a>
+            <a href="">&bull; Hosting</a>
+            <a href="">&bull; Messenger</a>
           </div>
         </div>
         <div className="footer-box">
